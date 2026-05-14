10e0d84d605ecdd79461ba765f8565c333242fde
Tarun007a <tarunchourey16@gmail.com>
2026-05-14 10:33:19 +0530

    [Tarun] : changed backed public ip to elastic ip

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor])),
     {
       provide: API_BASE_URL,
-      useValue: 'http://3.107.236.240:8080'
+      useValue: 'http://52.64.100.53:8080'
     }
   ]
 };
